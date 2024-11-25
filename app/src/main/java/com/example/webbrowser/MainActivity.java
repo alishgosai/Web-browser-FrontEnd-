13870ad1a48087cfd72b2f9032164b723bf45f1b
Alish Gosai <hellow6190@gmail.com>
2024-11-25 22:42:47 +1100

    webbrowser added

--- app/src/main/java/com/example/webbrowser/MainActivity.java
+++ app/src/main/java/com/example/webbrowser/MainActivity.java
@@ -1,24 +1,42 @@
 package com.example.webbrowser;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    private EditText urlBox;
+    private Button goButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Link UI components to Java code
+        webView = findViewById(R.id.webView);
+        urlBox = findViewById(R.id.urlBox);
+        goButton = findViewById(R.id.goButton);
+
+        // Enable JavaScript in the WebView
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        // Set WebViewClient to handle loading within the app
+        webView.setWebViewClient(new WebViewClient());
+
+        // Set the button click listener
+        goButton.setOnClickListener(view -> {
+            String url = urlBox.getText().toString();
+            if (!url.startsWith("https://") && !url.startsWith("http://")) {
+                url = "https://" + url; // Add "https://" if missing
+            }
+            webView.loadUrl(url);
         });
     }
 }
